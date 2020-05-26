d679c2be721632f848ff01e55598ea38b452694d
Ol Kovalenko <avk013@gmail.com>
2020-05-26 12:38:33 +0300

    Add files via upload
    тестовая заработавшая версия....

---
+++ test/pack.bat
@@ -0,0 +1,11 @@
+@echo off
+
+set source="C:\Date"
+set destination="C:\Temp"
+set passwd="Password"
+set dd=%DATE:~0,2%
+set mm=%DATE:~3,2%
+set yyyy=%DATE:~6,4%
+set curdate=%dd%-%mm%-%yyyy%
+
+"C:\Program Files\7-Zip\7z.exe" a -tzip -ssw -mx1 -p%passwd% -r0 %destination%\backup_%curdate%.zip %source%
